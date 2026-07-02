feb8d6aea50e12bfd0ce616064d445b88b3df411
Purnendu Tiwari <purnendutiwari2004@gmail.com>
2026-07-02 22:20:19 +0530

    feat : add submission worker

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { env } from "./config/env";
 import { submitRouter } from "./api/submit";
+import "./queue/submissionWorker";
 
 const app = express();
 
@@ -10,8 +11,7 @@ app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
-app.use('/', submitRouter)
-
+app.use("/", submitRouter);
 
 app.listen(env.PORT, () => {
   console.log(`RCE engine listening on port ${env.PORT}`);
--- src/queue/submissionWorker.ts
+++ src/queue/submissionWorker.ts
@@ -1,4 +1,25 @@
 import { Worker, Job } from "bullmq";
 import { redisConnection } from "../config/redis";
-import { submissionQueue } from "./submissionQueue";
+import { SUBMISSION_QUEUE_NAME } from "./submissionQueue";
 
+export const submissionWorker = new Worker(
+  SUBMISSION_QUEUE_NAME,
+  async (job: Job) => {
+    console.log('Processing job', job.id, job.data);
+
+    //dockerode sambhalega yaha pe
+
+    return { output: 'placeholder result' };
+  },
+  {
+    connection: redisConnection as any
+  }
+);
+
+submissionWorker.on('completed', (job) => {
+  console.log(`Job ${job.id} completed`);
+});
+
+submissionWorker.on('failed', (job, err) => {
+  console.error(`Job %{job?.id} failed :`, err.message);
+})
